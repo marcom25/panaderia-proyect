e2f102b6a9d0f224c7e394d9da2b80980a42aa89
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-02-04 19:01:19 -0300

    Adding sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,49 @@
 import React from "react";
-import "../Navbar.css";
 import "../Navbar";
 
 const Navbar = () => {
   return (
     <>
-      <div className="d-flex">
-        <div className="navbar navbar-dark bg-dark menu-icon">
-          <span className="navbar-toggler-icon" id="icono-menu"></span>
+      <nav class="navbar navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Offcanvas navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div
-          className="bg-dark"
-          style={{ textAlign: "center", width: "100%", color: "white" }}
-        >
-          Acá deberia ir la imagen jejox
-        </div>
-        <div className="bg-dark" style={{ color: "white" }}>
-          Changuito
-        </div>
-      </div>
-      <div className="cont-menu " id="menu">
-        <ul>
-          <li>Inicio</li>
-          <li>Tienda</li>
-          <li>Contacto</li>
-          <li>About us</li>
-          <li>Te estamos buscando</li>
-          <li>Ya no se me ocurre nada</li>
-        </ul>
-      </div>
+      </nav>
     </>
   );
 };
