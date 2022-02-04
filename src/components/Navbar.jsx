3b838fb46f4ff070324c403ba58fc41a8054c311
Marco Mignacco <marcomignacco03@gmail.com>
2022-02-04 20:22:05 -0300

    ada{

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,17 @@
 import React from "react";
 import "../Navbar";
 import "../Navbar.css";
+import Offcanvas from "./Offcanvas";
 
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-light bg-light fixed-top">
+      <nav class="navbar navbar-light bg-light sticky-top">
         <div class="container-fluid padding-left-jejox">
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Sobre nosotros</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contactanos</a>
-                </li>
-              </ul>
-              <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
+          <Offcanvas/>
         </div>
       </nav>
     </>
