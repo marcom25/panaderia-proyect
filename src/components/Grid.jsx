3eb8ee281eb51451b69a3c46d1fa82f977cc3731
Marco Mignacco <marcomignacco03@gmail.com>
2022-02-04 16:32:58 -0300

    dasda

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Card from "./Card";
+import { right } from '@popperjs/core';
 
 const Grid = () => {
     return ( 
-        <div className="container">
+        <div className="container" style={{padding: "0"}}>
             <div className='row'>
-                <div className='col'>
+                <div className='col-12'>
                     <Navbar/>
                 </div>
             </div>
