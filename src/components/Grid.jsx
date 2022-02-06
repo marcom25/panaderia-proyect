344d23748f257c8dc24fb6ceaf59f3f031f19580
Marco Mignacco <marcomignacco03@gmail.com>
2022-02-06 18:56:36 -0300

    dad

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,35 +3,40 @@ import Sidebar from "./Sidebar";
 import Navbar from './Navbar';
 import Card from "./Card";
 import ParallaxBg from './ParallaxBg';
-
+import "../css/Grid.css"
+// IMPORTS DE IMAGENES
+import image1 from "../images/donuts.jpg";
+import image2 from "../images/brownie.jpg";
+import image3 from "../images/medialunas.jpg"
 
 const Grid = () => {
     return ( 
         <div className="container-xs" style={{padding: "0"}}>
             <div className='row'>
-                <div className='col-12'>
+                <div className='col-12 p-0'>
                     <Navbar />
                     <Sidebar/>
                 </div>
             </div>
             <div className='row'>
-                <div className='col'>
+                <div className='col p-0'>
                     <ParallaxBg/> 
                 </div>
+                
             </div>
             <div className='row justify-content-sm-center'>
                 <div className='col-sm m-auto'>
-                    <Card/>
+                    <Card image={image1} />
                 </div>
-                <div className='col-sm'>
-                    <Card/>
+                <div className='col-sm p-0'>
+                    <Card image={image2} />
                 </div>
-                <div className='col-sm'>
-                    <Card/>
+                <div className='col-sm p-0'>
+                    <Card image={image3} />
                 </div>
             </div>
             <div className='row'>
-                <div className='col-sm'>
+                <div className='col-sm p-0'>
                     <div className=''>
                         <p>
                             TEXTO O IMAGENES
--- src/components/ParallaxBg.jsx
+++ src/components/ParallaxBg.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Parallax, Background} from 'react-parallax';
+import "../css/ParallaxBg.css"
 
 const ParallaxBg = () => (
     <Parallax
-        blur={{ min: -15, max: 15 }}
-        bgImage={require('../images/background-image-1.jpg')}
+        bgImage={require('../images/medialunas.jpg')}
         bgImageAlt="the dog"
         strength={-200}
     >
-        Blur transition from min to max
-        <div style={{ height: '10rem' }} />
+        <p className='paragraph'>Lorem pisum ddasdo dnakwndp </p>
+        <div className='sizer' />
     </Parallax>
 );
  
