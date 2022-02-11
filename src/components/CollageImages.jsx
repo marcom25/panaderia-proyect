801b4df7f5d91ef0488412cda60aee157a807fac
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-02-11 18:35:01 -0300

    Adding folder 'bakery'

--- src/components/CollageImages.jsx
+++ src/components/CollageImages.jsx
@@ -9,32 +9,32 @@ const CollageImages = () => {
     return (  
         <>
                
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={muffins} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={muffins} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={pan} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={pan} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={donuts} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={donuts} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={brownie} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={brownie} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={pancito} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={pancito} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={medialunas} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={medialunas} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={muffins} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={muffins} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={muffins} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={muffins} alt="muffins" className="w-100 h-100"/>
             </div>
-            <div className="col-md-4 col-12 p-4 p-md-2">
-                <img src={muffins} alt="muffins" className="w-100"/>
+            <div className="col-md-4 col-12 p-3 p-md-2">
+                <img src={muffins} alt="muffins" className="w-100 h-100"/>
             </div>
         
         </>
