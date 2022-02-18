cd3332ad2e382236d75abe7cdf9def36c54b6810
Marco Mignacco <marcomignacco03@gmail.com>
2022-02-18 20:04:06 -0300

    Route Trouble

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-
+import Products from './components/Products';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <
-        <Route path='/inicio' element={<Home/>} />
-        <Route path=''
-        
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='productos' element={<Products/>}/>
+    </Routes>
   );
 }
 
