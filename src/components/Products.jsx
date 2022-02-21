4bef26e8c2b64e9bda5222206a5a9fe81e6f56a4
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-02-21 13:45:08 -0300

    Trying to build a filter

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -31,168 +31,184 @@ const Products = () => {
                 <h3 className= 'py-4 text-center brown-font'>Productos</h3>
                 <div className='d-flex'>
                     <div className='d-none d-md-block ps-4 pe-5' style={{width: 'max-content'}}>
-                        <h5 className='brown-font font-weight-800'>Todos los productos</h5>
-                        <div>
-                            <h5 className='brown-font font-weight-800'>Pastelería</h5>
-                            <ul className='list-unstyled'>
-                                <li className='brown-font'>Brownie</li>
-                                <li className='brown-font'>Tortas</li>
-                                <li className='brown-font'>Masitas</li>
-                                <li className='brown-font'>Tartas</li>
-                                <li className='brown-font'>Macaroons</li>
-                                <li className='brown-font'>Muffins</li>
-                                <li className='brown-font'>Budines</li>
-                                <li className='brown-font'>Cheesecakes</li>
+                        <input type="radio" className="d-none" name="categories" id='Todo' value='Todo'/>
+                        <label className='brown-font line-height-products d-block mb-1 container-category' for="Todo" style={{fontWeight: '470'}}>Todos los productos</label>
+                        <hr className="m-0"/>
+                        <div className='filters'>
+                            <h5 className='brown-font mb-1' style={{fontSize: '1.1rem'}}>Pastelería</h5>
+                            <ul className='list-unstyled line-height-products mb-3'>
+                                <input type="radio" className="" name="categories" id="Brownies" value='Brownies' />
+                                <input type="radio" className="" name="categories" id="Tortas" value='Tortas'/>
+                                <input type="radio" className="d-none" name="categories" id="Masitas" value='Masitas'/>
+                                <input type="radio" className="d-none" name="categories" id="Tartas" value='Tartas'/>
+                                <input type="radio" className="d-none" name="categories" id="Macaroons" value='Macaroons'/>
+                                <input type="radio" className="d-none" name="categories" id="Muffins" value='Muffins'/>
+                                <input type="radio" className="d-none" name="categories" id="Budines" value='Budines'/>
+                                <input type="radio" className="d-none" name="categories" id="Cheesecakes" value='Cheesecakes'/>
+                                
+                                <label className='brown-font d-block container-category' for="Brownies">Brownies</label>
+                                <label className='brown-font d-block container-category' for="Tortas">Tortas</label>
+                                <label className='brown-font d-block container-category' for="Masitas">Masitas</label>
+                                <label className='brown-font d-block container-category' for="Tartas">Tartas</label>
+                                <label className='brown-font d-block container-category' for="Macaroons">Meriendas</label>
+                                <label className='brown-font d-block container-category' for="Muffins">Muffins</label>
+                                <label className='brown-font d-block container-category' for="Budines">Budines</label>
+                                <label className='brown-font d-block container-category' for="Cheesecakes">Cakes</label>
                             </ul>
                         </div>
                         <div>
-                            <h5>Bebidas calientes</h5>
-                            <ul className='list-unstyled'>
-                                <li>Café</li>
-                                <li>Té</li>
-                                <li>Chocolatada</li>
+                            <h5 className='line-height-products mb-1' style={{fontSize: '1.1rem'}}>Bebidas calientes</h5>
+                            <ul className='list-unstyled line-height-products'>
+
+                                <input type="radio" className="d-none" name="categories" id="Café" value='Café'/>
+                                <input type="radio" className="d-none" name="categories" id="Té" value='Té'/>
+                                <input type="radio" className="d-none" name="categories" id="Chocolatada" value='Chocolatada'/>
+
+                                <label className='brown-font d-block container-category' for="Café">Café</label>
+                                <label className='brown-font d-block container-category' for="Té">Té</label>
+                                <label className='brown-font d-block container-category' for="Cheesecakes">Chocolate</label>
                             </ul>
                         </div>
                     </div>
-                    <div className='d-flex flex-wrap'>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                    <div className='d-flex flex-wrap posts'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Brownies">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Brownies</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Tortas">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Tortas</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Masitas">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Masitas</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Tartas">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Tartas</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Macaroons">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Meriendas</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Muffins">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Muffins</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Budines">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Budines</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Cheesecakes">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Cakes</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Café">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Café</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Té">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Té</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Chocolatada">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Chocolatada</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
                                 </div>
                             </div>
                         </div>
-                        <div className='cardsWidth p-2 py-4 bg-cream'>
+                        <div className='cardsWidth p-2 py-4 bg-cream post' data-category="Chocolatada">
                             <div class="card bg-cream w-100 border-0">
                                 <img src={image1} class="card-img-top w-100 imgWidth" alt="..." />
                                 <div class="card-body bg-cream p-0 d-flex justify-content-between">
-                                    <h5 class="card-title brown-font">Pancakes</h5>
-                                    <p className='brown-font font-weight-800'>$500</p>
+                                    <h5 class="card-title brown-font">Chocolatada</h5>
+                                    <p className='brown-font font-weight-700'>$500</p>
                                 </div>
                                 <div className='m-auto'>
                                     <a href="#" class="btn cardsButton bg-brown white-font">+Añadir al carrito</a>
