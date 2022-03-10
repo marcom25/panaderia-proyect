92152bbb55c74527606d546b617d7cd7e31b6841
Diego <diego.nicolas.gandara@gmail.com>
2022-03-10 00:29:41 -0300

    feat: separate CardsProducts container component from SIngleProduct Card component

---
+++ src/$-products/CardsProducts.jsx
@@ -0,0 +1,27 @@
+import { CartState } from '../$-context/Context';
+
+import SingleProduct from './SingleProduct';
+
+const CardsProducts = ({ allCards, filteredCards }) => {
+  const {
+    state: { cart },
+  } = CartState();
+
+  const renderedCards = () => {
+    if (filteredCards.length === 0) {
+      return allCards?.map((card) => <SingleProduct card={card} />);
+    } else {
+      return filteredCards?.map((card) => <SingleProduct card={card} />);
+    }
+  };
+
+  console.log('cartState', cart);
+  return (
+    <div className="w-100">
+      <div className="w-100 d-flex flex-wrap">{renderedCards()}</div>
+      {<div></div>}
+    </div>
+  );
+};
+
+export default CardsProducts;
