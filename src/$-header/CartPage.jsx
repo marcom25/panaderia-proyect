f35ccb8d980e5666331e87623fb1662430643c6e
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-03-10 15:22:47 -0300

    Fixing errors in CartPage

--- src/$-header/CartPage.jsx
+++ src/$-header/CartPage.jsx
@@ -32,44 +32,47 @@ export const CartPage = () => {
 
   return (
     <>
-      <section className="container-xs bg-cream pt-2 pb-5 ">
-        <h2 className="px-2 my-0 text-uppercase">
-          <RiShoppingBag2Line style={{ paddingBottom: "0.1rem" }} /> Mi carrito
-        </h2>
-        <hr className="w-100 mt-3" />
-        <div className="row">
-          <div className="col-12 col-lg-8 p-0">
-            <div className="row">
-                <div className="col-12 col-lg-8 p-0">
-                    <div className="row">
-                        {cart.map((prod) => (
+        <section className="container-xs bg-cream pt-2 pb-5 ">
+            <h2 className="px-2 my-0 text-uppercase"><RiShoppingBag2Line style={{ paddingBottom: "0.1rem" }} /> Mi carrito</h2>
+            <hr className="w-100 mt-3" />
+            <div className="d-lg-flex justify-content-end">
+                <div className="row">
+                    <div className="col-12 p-0">
+                        <div className="row">
                             <div className="col-12 p-0">
                                 <div className="row">
-                                    <div className="col-4 ms-md-4 my-auto">
-                                        <img src={image} alt="pancake" className="w-100" />
-                                    </div>
-                                    <div className="col-4">
-                                        <h4>{prod.name}</h4>
-                                        <div className="d-flex">
-                                            <div className="me-3">
-                                                <p><span className="fw-bold">Precio:</span> ${prod.price}</p>
+                                    {cart.map((prod) => (
+                                        <div className="col-12 p-0">
+                                            <div className="row">
+                                                <div className="col-4 ms-md-4 my-auto">
+                                                    <img src={image} alt="pancake" className="w-100" />
+                                                </div>
+                                                <div className="col-4">
+                                                    <h4>{prod.name}</h4>
+                                                    <div className="d-flex">
+                                                        <div className="me-3">
+                                                            <p><span className="fw-bold">Precio:</span> ${prod.price}</p>
+                                                        </div>
+                                                        <div className="ps-3">
+                                                            <select class="form-select" aria-label="Default select example" onChange={(e) => dispatch({type: "CHANGE_CART_QTY", payload: {id: prod.id, qty: e.target.value,},})}>
+                                                                <option value="1">1</option>
+                                                                <option value="2">2</option>
+                                                                <option value="3">3</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="col-4 col-md-3 text-end ms-md-2">
+                                                    <button className="deleteIcon bg-cream fs-4" onClick={() => dispatch({type:"REMOVE_FROM_CART", payload: prod,})}><AiFillDelete className="deleteIcon brown-font"/></button>
+                                                </div>
                                             </div>
-                                            <div className="ps-3">
-                                                <select class="form-select" aria-label="Default select example" onChange={(e) => dispatch({type: "CHANGE_CART_QTY", payload: {id: prod.id, qty: e.target.value,},})}>
-                                                    <option value="1">1</option>
-                                                    <option value="2">2</option>
-                                                    <option value="3">3</option>
-                                                </select>
-                                            </div>
+                                            <hr className="w-100" />
                                         </div>
-                                    </div>
-                                    <div className="col-4 col-md-3 text-end ms-md-2">
-                                        <button className="deleteIcon bg-cream fs-4" onClick={() => dispatch({type:"REMOVE_FROM_CART", payload: prod,})}><AiFillDelete className="deleteIcon brown-font"/></button>
-                                    </div>
+                                    ))} 
                                 </div>
-                                <hr className="w-100" />
                             </div>
-                        ))} 
+                        </div>
+                    
                         <div className="col-12 col-lg-8 text-start p-0 d-lg-none px-3">
                             <h4 className="text-uppercase">Resumen de compra</h4>
                             <hr className="w-100 p-0 m-0" />
@@ -80,7 +83,7 @@ export const CartPage = () => {
                                 </div>
                                 <div>
                                     <p className="mb-0">$ {subTotal}</p>
-                                    <p>$ {subTotal * .21}</p>
+                                    <p>$ {subTotal * 0.21}</p>
                                 </div>
                             </div>
                             <hr className="w-100 p-0 m-0" />
@@ -89,139 +92,62 @@ export const CartPage = () => {
                                     <p>Total</p>
                                 </div>
                                 <div>
-                                <p>$ {total}</p>
+                                    <p>$ {total}</p>
                                 </div>
                             </div>
                             <div className="m-auto">
                                 <div className="w-100">
-                                    <button className="w-100 fs-5 buttons bg-brown white-font py-1" disabled={cart.length === 0}>Pagar</button>
+                                    <button
+                                    className="w-100 fs-5 returnButton font-poppins buttonPay"
+                                    disabled={cart.length === 0}
+                                    >
+                                    Pagar
+                                    </button>
                                 </div>
                             </div>
                         </div>
                         <hr className="w-100 mt-3 d-lg-none" />
                     </div>
-                    <div className="col-4">
-                      <h4>{prod.title}</h4>
-                      <div className="d-flex">
-                        <div className="me-3">
-                          <p>
-                            <span className="fw-bold">Precio:</span> $
-                            {prod.price}
-                          </p>
+                </div>
+                
+                <div className="col-12 col-lg-4 py-0 px-3 d-none d-lg-block">
+                    <div className="col-12 py-0 px-3 d-none d-lg-block">
+                        <h4 className="text-uppercase">Resumen de compra</h4>
+                        <hr className="w-100 p-0 m-0" />
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <p className="mb-0">Subtotal </p>
+                                <p>IVA +21%</p>
+                            </div>
+                            <div>
+                                <p className="mb-0">$ {subTotal}</p>
+                                <p>$ {subTotal * 0.21}</p>
+                            </div>
                         </div>
-                        <div className="ps-3">
-                          <select
-                            class="form-select"
-                            aria-label="Default select example"
-                            onChange={(e) =>
-                              dispatch({
-                                type: "CHANGE_CART_QTY",
-                                payload: { id: prod.id, qty: e.target.value },
-                              })
-                            }
-                          >
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                          </select>
+                        <hr className="w-100 p-0 m-0" />
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <p>Total</p>
+                            </div>
+                            <div>
+                                <p>$ {total}</p>
+                            </div>
                         </div>
-                      </div>
+                        <div className="m-auto">
+                            <div className="w-100 p-3">
+                                <button className="w-100 fs-5 returnButton font-poppins buttonPay" disabled={cart.length === 0}>Pagar</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-4 col-md-3 text-end ms-md-2">
-                      <button
-                        className="deleteIcon bg-cream fs-4"
-                        onClick={() =>
-                          dispatch({ type: "REMOVE_FROM_CART", payload: prod })
-                        }
-                      >
-                        <AiFillDelete className="deleteIcon brown-font" />
-                      </button>
-                    </div>
-                  </div>
-                  <hr className="w-100" />
-                </div>
-              ))}
-              <div className="col-12 col-lg-8 text-start p-0 d-lg-none px-3">
-                <h4 className="text-uppercase">Resumen de compra</h4>
-                <hr className="w-100 p-0 m-0" />
-                <div className="d-flex justify-content-between">
-                  <div>
-                    <p className="mb-0">Subtotal</p>
-                    <p>IVA +21%</p>
-                  </div>
-                  <div>
-                    <p className="mb-0">$ {subTotal}</p>
-                    <p>$ {subTotal * 0.21}</p>
-                  </div>
-                </div>
-                <hr className="w-100 p-0 m-0" />
-                <div className="d-flex justify-content-between">
-                  <div>
-                    <p>Total</p>
-                  </div>
-                  <div>
-                    <p>$ {total}</p>
-                  </div>
-                </div>
-                <div className="m-auto">
-                  <div className="w-100">
-                    <button
-                      className="w-100 fs-5 returnButton font-poppins buttonPay"
-                      disabled={cart.length === 0}
-                    >
-                      Pagar
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <hr className="w-100 mt-3 d-lg-none" />
+                </div> 
+            </div> 
+            <div className="text-center py-5">
+                <h4 className="pb-3">¿Deseas volver a la seccion productos?</h4>
+                <Link to="/productos">
+                    <button className="font-poppins returnButton">Volver a productos</button>
+                </Link>
             </div>
-          </div>
-          <div className="col-12 col-lg-4 py-0 px-3 d-none d-lg-block">
-            <div className="col-12 py-0 px-3 d-none d-lg-block">
-              <h4 className="text-uppercase">Resumen de compra</h4>
-              <hr className="w-100 p-0 m-0" />
-              <div className="d-flex justify-content-between">
-                <div>
-                  <p className="mb-0">Subtotal </p>
-                  <p>IVA +21%</p>
-                </div>
-                <div>
-                  <p className="mb-0">$ {subTotal}</p>
-                  <p>$ {subTotal * 0.21}</p>
-                </div>
-              </div>
-              <hr className="w-100 p-0 m-0" />
-              <div className="d-flex justify-content-between">
-                <div>
-                  <p>Total</p>
-                </div>
-                <div>
-                  <p>$ {total}</p>
-                </div>
-              </div>
-              <div className="m-auto">
-                <div className="w-100 p-3">
-                  <button
-                    className="w-100 fs-5 returnButton font-poppins buttonPay"
-                    disabled={cart.length === 0}
-                  >
-                    Pagar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="text-center py-5">
-            <h4 className="pb-3">¿Deseas volver a la seccion productos?</h4>
-            <Link to="/productos">
-              <button className="font-poppins returnButton">
-                Volver a productos
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section>
+        </section>
     </>
-  );
+  )
 };
