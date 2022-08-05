798c7c00f0d07d9c06e6019adb6413885d859c6b
Sebastian Ponce <sebaottok@gmail.com>
2022-08-05 01:14:54 -0300

    add: paginator in products section (react-paginate)

--- src/pages/Products/components/Products/Products.jsx
+++ src/pages/Products/components/Products/Products.jsx
@@ -1,18 +1,23 @@
 // IMPORTS DE MODULOS
-import React, { useEffect, useState } from 'react';
-import { CartState } from '../../../../contexts/Context';
-import { TitleProducts } from './Components';
+import React, { useEffect, useState, useCallback } from "react";
+import { CartState } from "../../../../contexts/Context";
+import { TitleProducts } from "./Components";
+import Paginator from "../Paginator/Paginator";
+
+// PAGINATOR
+import ReactPaginate from "react-paginate";
+import "../Paginator/css/Paginator.css";
 
 // IMPORTS DE COMPONENTES
-
 import {
   ButtonAllProducts,
   ButtonBakery,
   ButtonHotDrinks,
   ButtonsPagination,
-} from '../Buttons/Buttons.jsx';
-import CardsProducts from '../CardsProducts/CardsProducts.jsx';
-import LoaderPage from '../../../../components/common/LoaderPage/LoaderPage';
+} from "../Buttons/Buttons.jsx";
+import axiosClient from "../Paginator/api/axiosClient";
+import CardsProducts from "../CardsProducts/CardsProducts.jsx";
+import LoaderPage from "../../../../components/common/LoaderPage/LoaderPage";
 
 const Products = () => {
   const [allCategories, setAllCategories] = useState([]);
@@ -22,33 +27,35 @@ const Products = () => {
   const [products, setProducts] = useState([].slice(0, 12));
 
   const [sortedProducts, setSortedProducts] = useState([]);
-  
 
   const {
     filterState: { sort },
   } = CartState();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     // Refactor callback/promise hell for async/await
     const fetchProducts = async () => {
       try {
         setLoading(true);
-        const res = await fetch('https://panaderia-api.herokuapp.com/products', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Acces-Control-Allow_Origin': 'localhost:3000',
-          },
-          mode: 'cors',
-        });
+        const res = await fetch(
+          "https://panaderia-api.herokuapp.com/products",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Acces-Control-Allow_Origin": "localhost:3000",
+            },
+            mode: "cors",
+          }
+        );
         const { data } = await res.json();
         setProducts(data);
         setAllCategories([
-          'All',
+          "All",
           ...new Set(data.map((item) => item.category)),
         ]);
         setLoading(false);
@@ -60,16 +67,40 @@ const Products = () => {
   }, []);
 
   useEffect(() => {
-    if (sort == 'All') {
+    if (sort == "All") {
       setSortedProducts(products);
     } else setSortedProducts(products.filter((item) => item.category === sort));
+  }, [sort]);
 
-  }, [sort]); 
+  // PAGINATOR
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 8;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(products.slice(itemOffset, endOffset));
+    setPageCount(currentItems);
+    setPageCount(Math.ceil(products.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, products]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    setItemOffset(newOffset);
+    window.scrollTo(0, 0);
+  };
+
+  const resetPaginator = () => {
+    setCurrentItems(products.slice(0, 8));
+  };
 
   return (
     <>
       {!loading ? (
-        <section className="container-xs bg-cream" style={{ padding: '0' }}>
+        <section className="container-xs bg-cream" style={{ padding: "0" }}>
           <div className="container-xs bg-cream pt-2 pb-5">
             <TitleProducts
               className="text-center brown-font font-poppins fw-bold"
@@ -96,7 +127,9 @@ const Products = () => {
                 >
                   <ButtonAllProducts
                     buttons={allCategories}
+                    currentItems={currentItems}
                     className="fw-bold bg-brown-hover"
+                    onClick={() => setCurrentItems(products.slice(0, 8))}
                   />
                   <ButtonBakery buttons={allCategories} />
                   <ButtonHotDrinks buttons={allCategories} />
@@ -108,14 +141,16 @@ const Products = () => {
             <div className="d-flex">
               <div
                 className="d-none d-md-block ps-4 pe-5"
-                style={{ width: 'min-content' }}
+                style={{ width: "min-content" }}
               >
-                <ButtonAllProducts buttons={allCategories} />
+                <div onClick={() => resetPaginator()}>
+                  <ButtonAllProducts buttons={allCategories} />
+                </div>
                 <hr className="m-0" />
                 <div className="filters">
                   <h5
                     className="brown-font mb-1 font-poppins fw-bold"
-                    style={{ fontSize: '1.1rem' }}
+                    style={{ fontSize: "1.1rem" }}
                   >
                     Pastelería
                   </h5>
@@ -126,7 +161,7 @@ const Products = () => {
                 <div>
                   <h5
                     className="line-height-products mb-1 font-poppins fw-bold "
-                    style={{ fontSize: '1.1rem' }}
+                    style={{ fontSize: "1.1rem" }}
                   >
                     Bebidas calientes
                   </h5>
@@ -135,17 +170,36 @@ const Products = () => {
                   </ul>
                 </div>
               </div>
-              <div className="d-flex flex-wrap posts pe-lg-4 pe-xl-5 w-100" id="style-1">
+              <div
+                className="d-flex flex-wrap posts pe-lg-4 pe-xl-5 w-100"
+                id="style-1"
+              >
                 <CardsProducts
-                  allCards={products}
+                  allCards={currentItems}
                   filteredCards={sortedProducts}
                 />
               </div>
             </div>
+            <div className="m-auto mt-2">
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={2}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName="pagination"
+                pageLinkClassName="page-num"
+                previousLinkClassName="page-num"
+                nextLinkClassName="page-num"
+                activeLinkClassName="active"
+              />
+            </div>
           </div>
         </section>
       ) : (
-          <LoaderPage/>
+        <LoaderPage />
       )}
     </>
   );
